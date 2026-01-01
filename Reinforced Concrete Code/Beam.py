aec8cba8c87d05e9a6ad5beec33f5219d8f2eacd
Mrocha711 <michael.rocha1arce@gmail.com>
2026-01-01 12:47:39 -0800

    Added Beam Design Known Dimensions flexure only, still needs shear and deflection, and added time and notes to stats

--- Reinforced Concrete Code/Beam.py
+++ Reinforced Concrete Code/Beam.py
@@ -282,7 +282,7 @@ if procedure == "Analyzing":
             est = ecu*(d2-c)/c
             print("εs = εcu(d2-c)/c     " + str(ecu) + "(" + str(d2) + "-" + str(round(c,3)) + ")/" + str(round(c,3)) + " = " + str(round(est,5)))    
             if est >= ey + 0.003:
-                print("εt = " + str(round(es,5)) + " ≥ εy + 0.003 = " + str(round(ey,5)) + " + 0.003 = " + str(round(ey+0.003,5))+ " GOOD")
+                print("εt = " + str(round(est,5)) + " ≥ εy + 0.003 = " + str(round(ey,5)) + " + 0.003 = " + str(round(ey+0.003,5))+ " GOOD")
                 print("Therefore Φf = 0.9")
                 phif = 0.9
             else:
@@ -1038,8 +1038,262 @@ if procedure == "Analyzing":
             sys.exit()
         else:
             print("Δ_long ≤ Δ_long_allowed     " + str(round(del_long,3)) + " ≤ " + str(round(del_long_allowed,3)) + " Good the beam works")
-        
-        
+
+
+# desgin start
+elif procedure == "Desgining":
+    # Which version of a beam
+    question = "What type of beam"
+    options = ["Singely Reinforced Known Dimensions","Singely Reinforced Unknown Dimensions"]
+    if __name__ == "__main__":
+        beam_type = choice(question, options)
+    # Asking for all required values
+    if beam_type == "Singely Reinforced Known Dimensions":
+        Questions = ["f'c (psi)", "b (in)", "h (in)", "fy (ksi)", "Aggergate size", "Mu (kft)", "Stirrup Leg Size", "Number of legs"]
+        if __name__ == '__main__':
+            fpc, b, h, fy, dagg, Mu, Stirrup_size, Legs = get_inputs()
+        question = "What type of exposure"
+        options = ["No exposure to weather or ground", "Exposed to weather or ground", "Cast against and constant contact with ground"]
+        if __name__ == "__main__":
+            exposure = choice(question, options)
+        # find required area of steel
+        phif = 0.9
+        print("φf = 0.9")
+        if b <= h:
+            j = 0.9
+            print("j = 0.9")
+        else:
+            j = 0.95
+            print("j = 0.95")
+        if exposure == "No exposure to weather or ground":
+            cover = 1.5
+            print("Cover = 1.5 in")
+        elif exposure == "Exposed to weather or ground":
+            cover = 2
+            print("Cover = 2 in")
+        elif exposure == "Cast against and constant contact with ground":
+            cover = 3
+            print("Cover = 3 in")
+        d = h-cover-1
+        print("d = h-cover-1     " + str(h) + "-" + str(cover) + "-1 = " + str(d) + " in")
+        As_req = Mu*12/(phif*fy*j*d)
+        print("As_req = Mu*12/(φf*fy*j*d)     " + str(Mu) + "*12/" + str(phif) + "*" + str(fy) + "*" + str(j) + "*" + str(d) + " = " + str(round(As_req,3)) + " in^2")
+        df = pd.read_excel('Reinforced_Concrete_Tables.xlsx', sheet_name='Rebar_Size', engine='openpyxl') 
+        match = df[df["Bar number"] == Stirrup_size]
+        if not match.empty:
+            shear_rebar_diameter = match.iloc[0, 1]  
+            shear_rebar_area = match.iloc[0, 2]  
+        else:
+            print("The rebar size you typed is not in our list of rebar sizes.")
+            sys.exit()
+        flexure_rebar = [6, 7, 8, 9]
+        flexure_rebar_diameter = np.zeros(len(flexure_rebar))
+        flexure_rebar_area = np.zeros(len(flexure_rebar))
+        num_of_bars = np.zeros(len(flexure_rebar))
+        As = np.zeros(len(flexure_rebar))
+        rows = ["1","1","1","1"]
+        flexure_rebar_math = ["", "", "", ""]
+        s = np.zeros(len(flexure_rebar))
+        d = np.zeros(len(flexure_rebar))
+        d1 = np.zeros(len(flexure_rebar))
+        d2 = np.zeros(len(flexure_rebar))
+        c = np.zeros(len(flexure_rebar))
+        PhifMn = np.zeros(len(flexure_rebar))
+        dc_ratio = np.zeros(len(flexure_rebar))
+        for i in range(len(flexure_rebar)):
+            # finding area of rebar
+            df = pd.read_excel('Reinforced_Concrete_Tables.xlsx', sheet_name='Rebar_Size', engine='openpyxl') 
+            match = df[df["Bar number"] == flexure_rebar[i]]
+            if not match.empty:
+                flexure_rebar_diameter[i] = match.iloc[0, 1]  
+                flexure_rebar_area[i] = match.iloc[0, 2]  
+            else:
+                print("The rebar size you typed is not in our list of rebar sizes.")
+                sys.exit()
+            flexure_rebar_math[i] += "Rebar size: " + str(flexure_rebar[i])
+            flexure_rebar_math[i] += "\n"
+            num_of_bars[i] = As_req/flexure_rebar_area[i]
+            flexure_rebar_math[i] += "\n#_bars = " + str(round(As_req,3)) + "/" + str(flexure_rebar_area[i]) + " = " + str(round(num_of_bars[i],3))
+            num_of_bars[i] = round(num_of_bars[i]+0.5)
+            flexure_rebar_math[i] += "\nTherefore #_bars is = " + str(num_of_bars[i])
+            As[i] = flexure_rebar_area[i]*num_of_bars[i]
+            flexure_rebar_math[i] += "\nAs = " + str(flexure_rebar_area[i]) + "*" + str(num_of_bars[i]) + " = " + str(As[i]) + " in^2"
+            smin1 = 1
+            flexure_rebar_math[i] += "\nsmin1 = " + str(smin1) + " in"
+            smin2 = flexure_rebar_diameter[i]
+            flexure_rebar_math[i] += "\nsmin2 = d_b = " + str(smin2) + " in"
+            smin3 = 4/3*dagg
+            flexure_rebar_math[i] += "\nsmin2 = 4/3*dagg = 4/3" + str(dagg) + " = " + str(smin3) + " in"
+            smin = max(smin1,smin2,smin3)
+            flexure_rebar_math[i] += "\nTherefore smin = " + str(smin) + " in"
+            stirrup_bend = 2*shear_rebar_diameter-flexure_rebar_diameter[i]/2
+            flexure_rebar_math[i] += "\nstirrup_bend = 2*" + str(shear_rebar_diameter) + "-" + str(flexure_rebar_diameter[i]) + "/2 = " + str(round(stirrup_bend,3)) + " in"
+            b_min = 2*cover+Legs*shear_rebar_diameter+Legs*stirrup_bend+num_of_bars[i]*flexure_rebar_diameter[i]+(num_of_bars[i]-1)*smin
+            flexure_rebar_math[i] += "\nb_min = 2*" + str(cover) + "+" + str(Legs) + "*" + str(shear_rebar_diameter) + "+" + str(Legs) + "*" + str(round(stirrup_bend,3)) + "+" + str(num_of_bars[i]) + "*" + str(flexure_rebar_diameter[i]) + "+(" + str(num_of_bars[i]) + "-1)*" + str(smin) + " = " + str(round(b_min,3)) + " in"
+            if b_min > b:
+                flexure_rebar_math[i] += "\nb_min > b     " + str(round(b_min,3)) + " > " + str(b) + " Therefore need more than 1 row."
+                b_min = 4*cover+Legs*2*shear_rebar_diameter+Legs*2*stirrup_bend+num_of_bars[i]*flexure_rebar_diameter[i]+(num_of_bars[i]-1)*smin
+                flexure_rebar_math[i] += "\nb_min = 4*" + str(cover) + "+" + str(Legs) + "*2*" + str(shear_rebar_diameter) + "+" + str(Legs) + "*2*" + str(round(stirrup_bend,3)) + "+" + str(num_of_bars[i]) + "*" + str(flexure_rebar_diameter[i]) + "+(" + str(num_of_bars[i]) + "-1)*" + str(smin) + " = " + str(round(b_min,3)) + " in"
+                rows[i] = "2"
+            if b_min > 2*b:
+                flexure_rebar_math[i] += "\nb_min > 2*b     " + str(round(b_min,3)) + " > " + str(2*b) + " Therefore need more than 2 rows and code not set up for that."
+                rows[i] = "2+"
+            if rows[i] == "2+":
+                flexure_rebar_math[i] += "\n rows: " + rows[i] + " Therefore code cant continue from here."
+            else:
+                flexure_rebar_math[i] += "\n rows: " + rows[i]
+                smax1 = 15*40/(2/3*fy)-2.5*(cover+shear_rebar_diameter)
+                flexure_rebar_math[i] += "\nsmax1 = 15*40/(2/3*fy)-2.5(cover+d_s)     15*40/(2/3*" + str(fy) + ")-2.5(" + str(cover) + "+" + str(shear_rebar_diameter) + ") = " + str(round(smax1,3)) + " in"
+                smax2 = 12*40/(2/3*fy)
+                flexure_rebar_math[i] += "\nsmax2 = 12*40/(2/3*fy)     12*40/(2/3*" + str(fy) + ") = " + str(round(smax2,3)) + " in"
+                smax = min(smax1,smax2)
+                flexure_rebar_math[i] += "\nTherfore smax = " + str(round(smax,3)) + " in"
+                # Bar spacing could be upgraded probably should be but for know it is this
+                
+                
+                if rows[i] == "2":
+                    s[i] = smin+(b-b_min/2)/(round(num_of_bars[i]/2+0.5)-1)
+                    flexure_rebar_math[i] += "\ns = smin+(b-b_min)/(num_of_bars-1)     " + str(round(smin,3)) + "+(" + str(b) + "-" + str(round(b_min/2,3)) + ")/(" + str(round(num_of_bars[i]/2+0.5)) + "-1) = " + str(round(s[i],3)) + " in"
+                    num_of_bars[i] = round(num_of_bars[i]+0.5)
+                    flexure_rebar_math[i] += "\nChanging number of bars to work for code, previous bar count might work better though. = " + str(num_of_bars[i])
+                    As[i] = flexure_rebar_area[i]*num_of_bars[i]
+                    flexure_rebar_math[i] += "\nAs = " + str(flexure_rebar_area[i]) + "*" + str(num_of_bars[i]) + " = " + str(round(As[i],3)) + " in^2"
+                    d1[i] = h-cover-shear_rebar_diameter-0.5*flexure_rebar_diameter[i]
+                    flexure_rebar_math[i] += "\nd1 = h-cover-d_s-0.5*d_b     " + str(h) + "-" + str(cover) + "-" + str(shear_rebar_diameter) + "-0.5*" + str(flexure_rebar_diameter) + " = " + str(round(d1[i],3)) + " in"
+                    d1[i] = round(d1[i]-0.5)+0.5
+                    flexure_rebar_math[i] += "\nTherfore d1 = " + str(d1[i]) + " in"
+                    sminv = 1
+                    flexure_rebar_math[i] += "\nsminv = " + str(sminv) + " in"
+                    d2[i] = d1[i]-flexure_rebar_diameter[i]-sminv
+                    flexure_rebar_math[i] += "\nd2 = d1-d_b-sminv" + str(round(d1[i],3)) + "-" + str(flexure_rebar_diameter[i]) + "-" + str(sminv) + " = " + str(round(d2[i],3)) + " in"
+                    d2[i] = round(d2[i]-0.5)+0.5
+                    flexure_rebar_math[i] += "\nTherfore d2 = " + str(d2[i]) + " in"
+                    d[i] = As[i]/2*(d1[i]+d2[i])/As[i]
+                    flexure_rebar_math[i] += "\nd = As*(d1+d2)/As_t     " + str(As[i]/2) + "*(" + str(d1[i]) + "+" + str(d2[i]) + ")/" + str(As[i]) + " = " + str(d[i]) + " in"
+                elif rows[i] == "1":
+                    s[i] = smin+(b-b_min)/(num_of_bars[i]-1)
+                    flexure_rebar_math[i] += "\ns = smin+(b-b_min)/(num_of_bars-1)     " + str(round(smin,3)) + "+(" + str(b) + "-" + str(round(b_min,3)) + ")/(" + str(num_of_bars[i]) + "-1) = " + str(round(s[i],3)) + " in"
+                    d[i] = h-cover-shear_rebar_diameter-0.5*flexure_rebar_diameter[i]
+                    flexure_rebar_math[i] += "\nd = h-cover-d_s-0.5*d_b     " + str(h) + "-" + str(cover) + "-" + str(shear_rebar_diameter) + "-0.5*" + str(flexure_rebar_diameter[i]) + " = " + str(round(d[i],3)) + " in"
+                    d[i] = round(d[i]-0.5)+0.5 #rounding down to nearest half theoreticly
+                    d1[i] = d[i]
+                    d2[i] = d[i]
+                    flexure_rebar_math[i] += "\nTherfore d = " + str(d[i]) + " in"
+                flexure_rebar_math[i] += "\nChecking if it works."
+                #Finding c and a
+                if fpc <= 4000:
+                    flexure_rebar_math[i] += "\nf'c ≤ 4000 so β1 = 0.85"
+                    beta1 = 0.85
+                elif fpc >= 8000:
+                    flexure_rebar_math[i] += "\nf'c ≥ 8000 so β1 = 0.65"
+                    beta1 = 0.65
+                else:
+                    flexure_rebar_math[i] += "\n4000 ≤ f'c ≤ 8000"
+                    beta1 = 0.85 - (0.05*(fpc-4000)/1000)
+                    flexure_rebar_math[i] += "\nβ1 = 0.85-0.05(f'c-4000)/1000     0.85-0.05(" + str(fpc) + "-4000)/1000 = " + str(round(beta1,3))
+                c[i] = As[i]*fy*1000/(0.85*fpc*beta1*b)
+                flexure_rebar_math[i] += "\nc = As*fy*1000/(0.85*f'c*β1*b)     " + str(As[i]) + "*" + str(fy) + "*1000/(0.85*" + str(fpc) + "*" + str(round(beta1,3)) + "*" + str(b) + ") = " + str(round(c[i],3)) + " in"
+                a = beta1*c[i]
+                flexure_rebar_math[i] += "\na = β1*c     " + str(round(beta1,3)) + "*" + str(round(c[i],3)) + " = " + str(round(a,3)) + " in"
+                # checking strain
+                if rows[i] == "2":
+                    ecu = 0.003
+                    flexure_rebar_math[i] += "\nεcu = 0.003"
+                    es = ecu*(d2[i]-c[i])/c[i]
+                    flexure_rebar_math[i] += "\nεs = εcu(d2-c)/c     " + str(ecu) + "(" + str(d2[i]) + "-" + str(round(c[i],3)) + ")/" + str(round(c[i],3)) + " = " + str(round(es,5))
+                    ey = fy/29000
+                    flexure_rebar_math[i] += "\nεy = fy/29000     " + str(fy) + "/29000 = " + str(round(ey,5))
+                    if es >= ey:
+                        flexure_rebar_math[i] += "\nεs = " + str(round(es,5)) + " ≥ εy = " + str(round(ey,5)) + " GOOD"
+                        est = ecu*(d1[i]-c[i])/c[i]
+                        flexure_rebar_math[i] += "\nεs = εcu(d1-c)/c     " + str(ecu) + "(" + str(d1[i]) + "-" + str(round(c[i],3)) + ")/" + str(round(c[i],3)) + " = " + str(round(est,5))   
+                        if est >= ey + 0.003:
+                            flexure_rebar_math[i] += "\nεt = " + str(round(est,5)) + " ≥ εy + 0.003 = " + str(round(ey,5)) + " + 0.003 = " + str(round(ey+0.003,5))+ " GOOD"
+                            flexure_rebar_math[i] += "\nTherefore Φf = 0.9"
+                            phif = 0.9
+                            PhifMn[i] = phif*As[i]*fy*(d[i]-a/2)/12
+                            flexure_rebar_math[i] += "\nΦfMn = Φf*As*fy*(d-a/2)/12     " + str(phif) + "*" + str(As[i]) + "*" + str(fy) + "*(" + str(d[i]) + "-" + str(round(a,3)) + "/2)/12 = " + str(round(PhifMn[i],3)) + " kft"
+                            dc_ratio[i] = Mu/PhifMn[i]
+                            flexure_rebar_math[i] += "\ndc_ratio = Mu/ΦfMn     " + str(Mu) + "/" + str(round(PhifMn[i],3)) + " = " + str(round(dc_ratio[i],3))
+                            # Checking Asmin
+                            Asmin1 = 3*(fpc)**(1/2)/(fy*1000)*b*d[i]
+                            flexure_rebar_math[i] += "\nAsmin1 = 3√(fpc)/(fy*1000)*b*d     3√(" + str(fpc) + ")/(" + str(fy) + "*1000)*" + str(b) + "*" + str(d[i]) + " = " + str(round(Asmin1,3)) + " in^2"
+                            Asmin2 = 200/(fy*1000)*b*d[i]
+                            flexure_rebar_math[i] += "\nAsmin2 = 200/(fy*1000)*b*d     200/(" + str(fy) + "*1000)*" + str(b) + "*" + str(d[i]) + " = " + str(round(Asmin2,3)) + " in^2"
+                            Asmin = max(Asmin1, Asmin2)
+                            flexure_rebar_math[i] += "\nAsmin = " + str(round(Asmin,3)) + " in^2"
+                            if As[i] >= Asmin:
+                                flexure_rebar_math[i] += "\nAs = " + str(round(As[i],3)) + " ≥ Asmin = " + str(round(Asmin,3)) + " GOOD"
+                            else:
+                                flexure_rebar_math[i] += "\nAs = " + str(round(As[i],3)) + " ≥ Asmin = " + str(round(Asmin,3)) + " NO Beam fails"
+                        else:
+                            flexure_rebar_math[i] += "\nεt = " + str(round(es,5)) + " ≥ εy + 0.003 = " + str(round(ey,5)) + " + 0.003 = " + str(round(ey+0.003,5))+ " NO"
+                            flexure_rebar_math[i] += "\nFails Φf = 0.9"
+                    else:
+                        flexure_rebar_math[i] += "\εs = " + str(round(es,5)) + " ≥ εy = " + str(round(ey,5)) + " NO, and wont pass Φf = 0.9 since εt ≥ ey + 0.003"
+                        #Note possible it might pass if d is big enough but doutful and at that point probably closer to double reinforced.
+                        #leaving space and note incase we decide to add it.
+                elif rows[i] == "1":
+                    ecu = 0.003
+                    flexure_rebar_math[i] += "\nεcu = 0.003"
+                    es = ecu*(d[i]-c[i])/c[i]
+                    flexure_rebar_math[i] += "\nεs = εcu(d-c)/c     " + str(ecu) + "(" + str(d[i]) + "-" + str(round(c[i],3)) + ")/" + str(round(c[i],3)) + " = " + str(round(es,5))
+                    ey = fy/29000
+                    flexure_rebar_math[i] += "\nεy = fy/29000     " + str(fy) + "/29000 = " + str(round(ey,5))
+                    if es >= ey:
+                        flexure_rebar_math[i] += "\nεs = " + str(round(es,5)) + " ≥ εy = " + str(round(ey,5)) + " GOOD"
+                        if es >= ey + 0.003:
+                            flexure_rebar_math[i] += "\nεt = " + str(round(es,5)) + " ≥ εy + 0.003 = " + str(round(ey,5)) + " + 0.003 = " + str(round(ey+0.003,5))+ " GOOD"
+                            flexure_rebar_math[i] += "\nTherefore Φf = 0.9"
+                            phif = 0.9
+                            PhifMn[i] = phif*As[i]*fy*(d[i]-a/2)/12
+                            flexure_rebar_math[i] += "\nΦfMn = Φf*As*fy*(d-a/2)/12     " + str(phif) + "*" + str(As[i]) + "*" + str(fy) + "*(" + str(d[i]) + "-" + str(round(a,3)) + "/2)/12 = " + str(round(PhifMn[i],3)) + " kft"
+                            dc_ratio[i] = Mu/PhifMn[i]
+                            flexure_rebar_math[i] += "\ndc_ratio = Mu/ΦfMn     " + str(Mu) + "/" + str(round(PhifMn[i],3)) + " = " + str(round(dc_ratio[i],3))
+                            # Checking Asmin
+                            Asmin1 = 3*(fpc)**(1/2)/(fy*1000)*b*d[i]
+                            flexure_rebar_math[i] += "\nAsmin1 = 3√(fpc)/(fy*1000)*b*d     3√(" + str(fpc) + ")/(" + str(fy) + "*1000)*" + str(b) + "*" + str(d[i]) + " = " + str(round(Asmin1,3)) + " in^2"
+                            Asmin2 = 200/(fy*1000)*b*d[i]
+                            flexure_rebar_math[i] += "\nAsmin2 = 200/(fy*1000)*b*d     200/(" + str(fy) + "*1000)*" + str(b) + "*" + str(d[i]) + " = " + str(round(Asmin2,3)) + " in^2"
+                            Asmin = max(Asmin1, Asmin2)
+                            flexure_rebar_math[i] += "\nAsmin = " + str(round(Asmin,3)) + " in^2"
+                            if As[i] >= Asmin:
+                                flexure_rebar_math[i] += "\nAs = " + str(round(As[i],3)) + " ≥ Asmin = " + str(round(Asmin,3)) + " GOOD"
+                            else:
+                                flexure_rebar_math[i] += "\nAs = " + str(round(As[i],3)) + " ≥ Asmin = " + str(round(Asmin,3)) + " NO Beam fails"
+                        else:
+                            flexure_rebar_math[i] += "\nεt = " + str(round(es,5)) + " ≥ εy + 0.003 = " + str(round(ey,5)) + " + 0.003 = " + str(round(ey+0.003,5))+ " NO"
+                            flexure_rebar_math[i] += "\nFails Φf = 0.9"
+                    else:
+                        flexure_rebar_math[i] += "\nεs = " + str(round(es,5)) + " ≥ εy = " + str(round(ey,5)) + " NO, and wont pass Φf = 0.9 since εt ≥ ey + 0.003"
+        #printing all math for Flexure check
+        for i in range(len(flexure_rebar)):
+            print(flexure_rebar_math[i])
+        #quick overview
+        for i in range(len(flexure_rebar)):
+            print("dc_ratio of " + str(num_of_bars[i]) + " #" + str(flexure_rebar[i]) + "bars: " + str(round(dc_ratio[i],3)))
+                        
+                        
+                        
+                        
+                        
+                    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
