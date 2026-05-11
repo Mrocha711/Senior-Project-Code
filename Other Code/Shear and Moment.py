4e6e574ff9ecc4e0f53d3a3f49fb099628c06cdc
Mrocha711 <michael.rocha1arce@gmail.com>
2026-05-11 13:59:20 -0700

    Working on getting shear to work with new x line of beam, still not working

--- Other Code/Shear and Moment.py
+++ Other Code/Shear and Moment.py
@@ -4,21 +4,55 @@ Created on Fri Apr  3 10:21:16 2026
 
 @author: victo
 """
-p = 0
-w = 2
-L= 13
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-def newmarks_average( beam_type, p, w, L):
-    #creating length array of the beam at every inch of the beam
+
+#test varriables
+L = 2
+P = np.array([1])
+P_L = np.array([1])
+W = np.array([])
+W_L = np.array([])
+
+def Simply_Supported(P, P_L, W, W_L, L):
+    # getting all inch values along the beam
     number_of_x = round(L*12+1)
     x_line = np.zeros(number_of_x)
-    for i in range(number_of_x):
+    for i in range(len(x_line)):
         x_line[i] = i/12
-        if i == number_of_x-1:
-            x_line[i] = L
-    #Solving for V and M of respected beam type across all of the beam
+    # combining all the distances for loads into one array
+    Load_L = np.append([P_L], [W_L])
+    Load_L = np.append(np.unique([Load_L]), np.unique([P_L]))
+    
+    # combining the lengtharray with all the distance arrays for loads to have all important points.
+    x_line = np.append([x_line],[Load_L])
+
+    # sorting all the distances into numerical order
+    x_line = np.sort([x_line])
+    
+    # combining multiple arrays creates a 2D array need it to be a 1D array
+    x_line = x_line.flatten()
+    
+    #creating arrays to hold values of shear moment and deflection
+    V = np.zeros(len(x_line))
+    M = np.zeros(len(x_line))
+    
+    #Solving for if it has any point loads on the beam
+    for i in range(len(P)):
+        counter = 0
+        for y in range(len(x_line)):
+            if x_line[y] <= P_L[i] and counter == 0:
+                V[i] = P[i]/2
+            else:
+                V[i] = -P[i]/2
+    
+    
+    
+    
+    
+    
+    """#Solving for V and M of respected beam type across all of the beam
     V = np.zeros(number_of_x)
     M = np.zeros(number_of_x)
     if beam_type == "Simply Supported":
@@ -26,31 +60,31 @@ def newmarks_average( beam_type, p, w, L):
         for i in range(number_of_x):
             # Equation for point Shear @ mid
             if (x_line[i] < L/2):
-                V[i] = p/2
+                V[i] = P/2
             elif (x_line[i] > L/2):
-                V[i] = -p/2
+                V[i] = -P/2
             #solving for Moment for point load @ mid
             if (x_line[i] < L/2):
-                M[i] = p*x_line[i]/2
+                M[i] = P*x_line[i]/2
             elif (x_line[i] > L/2):
-                M[i] = p*(L-x_line[i])/2
+                M[i] = P*(L-x_line[i])/2
             elif (x_line[i] == L/2):
-                M[i] = p*L/4
+                M[i] = P*L/4
             # Equation for unit load shear along whole beam
-            V[i] += w*(L/2-x_line[i])
+            V[i] += W*(L/2-x_line[i])
             # Equation for unit load Moment along whole beam
-            M[i] += w*x_line[i]/2*(L-x_line[i])
+            M[i] += W*x_line[i]/2*(L-x_line[i])
     elif beam_type == "Cantilever":
             #solving
             for i in range(number_of_x):
                 # Equation for point Shear @ tip
-                V[i] = -p
+                V[i] = -P
                 #solving for Moment for point load @ tip
-                M[i] = p*x_line[i]
+                M[i] = P*x_line[i]
                 # Equation for unit load shear along whole beam
-                V[i] += -w*x_line[i]
+                V[i] += -W*x_line[i]
                 # Equation for unit load Moment along whole beam
-                M[i] += w*x_line[i]**2/2
+                M[i] += W*x_line[i]**2/2
     
     #Shear plot
     plt.plot(x_line, V, color='blue', linestyle='-',)
@@ -73,20 +107,21 @@ def newmarks_average( beam_type, p, w, L):
 
 L = 2
 P_L = np.array([1,1.5])
-
+W_L = np.array([0.5,2])
 # getting all inch values along the beam
 number_of_x = round(L*12+1)
 x_line = np.zeros(number_of_x)
 for i in range(len(x_line)):
     x_line[i] = i/12
 # combining all the distances for loads into one array
-Load_L = np.append([P_L], [1])
-Load_L = np.unique([Load_L])
+Load_L = np.append([P_L], [W_L])
+Load_L = np.append(np.unique([Load_L]), np.unique([P_L]))
 
 # combining the lengtharray with all the distance arrays for loads to have all important points.
-x_line = np.append([x_line],[P_L])
+x_line = np.append([x_line],[Load_L])
+
 # sorting all the distances into numerical order
-x_line = np.sort([x_line])
+x_line = np.sort([x_line])"""
 
 
 
