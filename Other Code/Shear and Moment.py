658ac966e7254866ebfe58b6bf09ea7f50f31821
Mrocha711 <michael.rocha1arce@gmail.com>
2026-09-05 17:17:04 -0700

    I dont know what is diffrent here, Just have a push to do

--- Other Code/Shear and Moment.py
+++ Other Code/Shear and Moment.py
@@ -188,3 +188,100 @@ x_line = np.sort([x_line])"""
 
 
 
+
+
+
+
+
+
+
+
+
+
+    """#Solving for V and M of respected beam type across all of the beam
+    V = np.zeros(number_of_x)
+    M = np.zeros(number_of_x)
+    if beam_type == "Simply Supported":
+        #solving
+        for i in range(number_of_x):
+            # Equation for point Shear @ mid
+            if (x_line[i] < L/2):
+                V[i] = P/2
+            elif (x_line[i] > L/2):
+                V[i] = -P/2
+            #solving for Moment for point load @ mid
+            if (x_line[i] < L/2):
+                M[i] = P*x_line[i]/2
+            elif (x_line[i] > L/2):
+                M[i] = P*(L-x_line[i])/2
+            elif (x_line[i] == L/2):
+                M[i] = P*L/4
+            # Equation for unit load shear along whole beam
+            V[i] += W*(L/2-x_line[i])
+            # Equation for unit load Moment along whole beam
+            M[i] += W*x_line[i]/2*(L-x_line[i])
+    elif beam_type == "Cantilever":
+            #solving
+            for i in range(number_of_x):
+                # Equation for point Shear @ tip
+                V[i] = -P
+                #solving for Moment for point load @ tip
+                M[i] = P*x_line[i]
+                # Equation for unit load shear along whole beam
+                V[i] += -W*x_line[i]
+                # Equation for unit load Moment along whole beam
+                M[i] += W*x_line[i]**2/2
+    
+    #Shear plot
+    plt.plot(x_line, V, color='blue', linestyle='-',)
+    # Add title and labels
+    plt.title("Shear Diagram")
+    plt.xlabel("Length (ft)")
+    plt.ylabel("Shear (K)")
+    # Show the plot
+    plt.show()
+    #moment plot
+    plt.plot(x_line, M, color='blue', linestyle='-',)
+    # Add title and labels
+    plt.title("Moment Diagram")
+    plt.xlabel("Length (ft)")
+    plt.ylabel("Moment (K-ft)")
+    # Show the plot
+    plt.show()      
+    return V, M
+
+
+L = 2
+P_L = np.array([1,1.5])
+W_L = np.array([0.5,2])
+# getting all inch values along the beam
+number_of_x = round(L*12+1)
+x_line = np.zeros(number_of_x)
+for i in range(len(x_line)):
+    x_line[i] = i/12
+# combining all the distances for loads into one array
+Load_L = np.append([P_L], [W_L])
+Load_L = np.append(np.unique([Load_L]), np.unique([P_L]))
+
+# combining the lengtharray with all the distance arrays for loads to have all important points.
+x_line = np.append([x_line],[Load_L])
+
+# sorting all the distances into numerical order
+x_line = np.sort([x_line])"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
